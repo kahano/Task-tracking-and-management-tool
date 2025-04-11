7642f6700244aaf68541a41188273c37398f9fdd
Mohamed Ali <kahano243@gmail.com>
2025-04-11 17:40:29 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/kahano/Task/tracking/and/management/tool/Services/Impl/TaskServiceImpl.java
    #	src/main/java/com/kahano/Task/tracking/and/management/tool/Services/TaskService.java

--- src/main/java/com/kahano/Task/tracking/and/management/tool/Services/Impl/TaskServiceImpl.java
+++ src/main/java/com/kahano/Task/tracking/and/management/tool/Services/Impl/TaskServiceImpl.java
@@ -1,4 +1,33 @@
 package com.kahano.Task.tracking.and.management.tool.Services.Impl;
 
-public class TaskServiceImpl {
+import com.kahano.Task.tracking.and.management.tool.Repository.TaskRespository;
+import com.kahano.Task.tracking.and.management.tool.Services.TaskService;
+import com.kahano.Task.tracking.and.management.tool.domain.entities.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRespository taskRepository;
+    public TaskServiceImpl(TaskRespository taskRepository) {
+        this.taskRepository = taskRepository;
+
+
+    }
+
+    @Override
+    public List<Task> getAllTask(UUID taskSetId) {
+        return taskRepository.findByTaskSetId(taskSetId);
+    }
+
+    @Override
+    public Task getTaskById(UUID taskSetId, UUID taskId) {
+        Optional<Task> task = taskRepository.findByTaskSetIdAndTaskId(taskSetId, taskId);
+        return task.isPresent() ? task.get() : null;
+
+    }
 }
